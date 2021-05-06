2aefcdf20cd39bf1651f926d4163cfe75892d706
centraldogma99 <53252434+centraldogma99@users.noreply.github.com>
2021-05-06 22:24:30 +0900

    Change some bad codes

--- security.js
+++ security.js
@@ -4,3 +4,4 @@ module.exports.mongo = process.env.DB_PATH;
 module.exports.session = process.env.SESSION_KEY;
 module.exports.sparcssso_id = process.env.SPARCSSSO_CLIENT_ID;
 module.exports.sparcssso_key = process.env.SPARCSSSO_CLIENT_KEY;
+module.exports.nodePort = process.env.NODE_PORT;
