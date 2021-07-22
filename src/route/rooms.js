6521ac3bccb81a78d9d04e7f25bc780841008486
central.dogma99 <central.dogma99@gmail.com>
2021-07-23 04:38:40 +0900

    change return formats/fix minor bugs with rooms

--- src/route/rooms.js
+++ src/route/rooms.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { stringify } = require('querystring');
-const { locationModel } = require('../db/mongo');
 const router = express.Router();
 const loginCheckMiddleware = require('../middleware/logincheck')
+//const taxiResponse = require('../taxiResponse')
+
 
 module.exports = (mongo) => {
   router.get('/', function(_, _) {
@@ -46,97 +46,126 @@ module.exports = (mongo) => {
   // part : Array
   router.post('/create', async (req, res) => {
     //console.log(req.body);
-    let from = await mongo.locationModel.findOneAndUpdate({"name":req.body.from},{}, {new:true, upsert:true}).exec()
-    console.log(from)
-   	let to = await mongo.locationModel.findOneAndUpdate({"name":req.body.to},{}, {new:true, upsert:true}).exec()
-    
-    let room = new mongo.roomModel({
-      'name':req.body.name,
-      'from':from._id,
-      'to':to._id,
-      'time':req.body.time,
-      'part':req.body.part,
-      'madeat':Date.now()
-    })
-    room.save()
-    .then(room => {
-      res.status(200).send('Rooms/create : add room successful')
-    })
-    .catch(err => {
-      res.status(500).send('Rooms/create : internal server error');
+    if(!req.body.name || !req.body.from || !req.body.to || !req.body.time ){
+      res.status(400).json({
+        error : "Rooms/create : bad request"
+      })
+    }
+
+    try {
+      let from = await mongo.locationModel.findOneAndUpdate(
+        {"name":req.body.from},
+        {},
+        {new : true, upsert : true}
+      )
+      let to = await mongo.locationModel.findOneAndUpdate(
+        {"name":req.body.to},
+        {},
+        {new : true, upsert : true}
+      )
+      
+      let room = new mongo.roomModel({
+        'name':req.body.name,
+        'from':from._id,
+        'to':to._id,
+        'time':req.body.time,
+        'part':req.body.part,
+        'madeat':Date.now()
+      })
+      await room.save()
+      res.send(room);
+    } catch(err) {
       console.log(err);
-    });
+      res.status(500).json({
+        error : 'Rooms/create : internal server error'
+      })
+    }
   });
 
-  router.get('/:id/delete', (req, res) => {
-    console.log("room delete : "+req.params.id)
-    mongo.roomModel.findByIdAndRemove( req.params.id )
-    .then( result => {
+  router.get('/:id/delete', async (req, res) => {
+    try {
+      let result;
+      result = await mongo.roomModel.findByIdAndRemove( req.params.id )
       if(result) {
-        console.log(result);
-        res.status(200).send('Rooms/delete : delete room successful')
+        res.send(true);
       } else {
-        console.log("room delete error : id does not exist");
-        res.status(404).send("Rooms/delete : ID does not exist");
+        res.status(404).json({
+          error : "Rooms/delete : ID does not exist"
+        });
       }
-    })
-    // catch는 반환값이 없을 경우(result == undefined일 때)는 처리하지 않는다.
-    .catch( error => {
-      console.log(error);
-      res.status(500).send("Rooms/delete : internal server error")
-    })
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        error : 'Rooms/create : internal server error'
+      })
+    }
     
+    // catch는 반환값이 없을 경우(result == undefined일 때)는 처리하지 않는다.
   })
 
   // json으로 수정할 값들을 받는다
   // json 형식이 맞는지 검증해야 함
   // request JSON
   // name, from, to, time, part
-  router.post('/:id/edit', (req, res) => {
-    if( !req.body.name || !req.body.from || !req.body.to || !req.body.time || !req.body.part )
-      res.status("400").send("Rooms/edit : Bad request")
-    mongo.roomModel.findByIdAndUpdate(req.params.id, {$set : {
+  router.post('/:id/edit', async (req, res) => {
+    if( !req.body.name || !req.body.from || !req.body.to || !req.body.time || !req.body.part ){
+      res.status("400").json({
+        error : "Rooms/edit : Bad request"
+      })
+    }
+    let from = await mongo.locationModel.findOneAndUpdate(
+      { "name" : req.body.from },
+      {},
+      { new : true, upsert : true }
+    )
+    let to = await mongo.locationModel.findOneAndUpdate(
+      { "name" : req.body.to },
+      {},
+      { new : true, upsert : true }
+    )
+    changeJSON = {
       name: req.body.name,
-      from: req.body.from,
-      to: req.body.to,
+      from: from,
+      to: to,
       time: req.body.time,
       part: req.body.part,
-    }})
-    .then( result => {
-      if(result){
-        console.log("edit room successful")
-        res.status(200).send('Rooms/edit : edit room successful')
+    }
+    
+    try {
+      let result = await mongo.roomModel.findByIdAndUpdate(req.params.id, {$set : changeJSON, new : true});
+      console.log(result);
+      if (result){
+        res.send(changeJSON);
       } else {
-        console.log("room delete error : id does not exist");
-        res.status(404).send("Rooms/edit : id does not exist");
+        res.status(404).json({
+          error : "Rooms/edit : id does not exist"
+        });
       }
-    })
-    .catch( error => {
-      console.log("room edit error : " +error);
-      res.status(500).send("Rooms/edit : internal server error")
-    })
+    } catch(err) {
+      console.log(err);
+      res.status(500).json({
+        error : "Rooms/edit : internal server error"
+      })
+    }
   })
 
   // 특정 id 방 세부사항 보기
-  router.get('/:id', (req, res) => {
-    mongo.roomModel.findById( req.params.id )
-    .then( result => {
-        if(result) {
-          console.log(JSON.stringify(result))
-          res.status(200).send(JSON.stringify({
-            error : false,
-            message : "Rooms/info : room info successful",
-            data : result
-          }));
-        } else {
-          console.log("room info error : id does not exist")
-          res.status(404).send("Rooms/info : id does not exist");
-        }
-      })
-    .catch( err => { 
+  router.get('/:id', async (req, res) => {
+    try {
+      let result = await mongo.roomModel.findById( req.params.id )
+      if(result) {
+        res.status(200).send( result );
+      } else {
+        res.status(404).json({
+          error : "Rooms/info : id does not exist"
+        });
+      }
+    } catch (err) {
       console.log(err);
-      res.status(500).send("Rooms/info : internal server error")
-    })
+      res.status(500).json({
+        error : "Rooms/info : internal server error"
+      })
+    }
   })
 
   // test method
@@ -162,25 +191,32 @@ module.exports = (mongo) => {
   //   users : List[ObjectID] }
   router.post('/invite', async (req, res) => {
     // Request JSON Validation
-    if( !req.body.roomId || !req.body.users ) res.status("400").send("Room/invite : Bad request")
-
+    if( !req.body.roomId || !req.body.users ) res.status("400").json({
+      error : "Room/invite : Bad request"
+    })
     try {
       let room = await mongo.roomModel.findById( req.body.roomId )
-      if ( !room ) res.status(404).send("Room/invite : no corresponding room")
+      if ( !room ) res.status(404).json({
+        error : "Room/invite : no corresponding room"
+      })
       for( const userID of req.body.users ){
-        if ( room.part.includes(userID) ) res.status("409").send("Room/invite : "+ userID +" Already in room")
+        if ( room.part.includes(userID) ) res.status("409").json({
+          error : "Room/invite : "+ userID +" Already in room"
+        })
       }
-      await room.save()
       for ( const userID of req.body.users ){
-        room.part.append(userID)
+        room.part = room.part.concat(userID)
+        await room.save()
         let user = await mongo.userModel.findById( userID )
-        user.room.append( req.body.roomId )
-        await user.save()
+        user.room = user.room.concat( req.body.roomId )
+        user.save()
       }
-      res.status("200").send("Room/invite : invite successful")
+      res.send(room);
     } catch ( error ) {
       console.log(error);
-      res.status("500").send("Room/invite : internal server error")
+      res.status("500").json({
+        error : "Room/invite : internal server error"
+      })
     }
   })
 
@@ -195,10 +231,9 @@ module.exports = (mongo) => {
   // startDate는 선택
   router.post('/search', async (req, res) => {
     if ( !req.body.fromName && !req.body.toName ) {
-      res.status("400").send(JSON.stringify({
-        error : true,
-        message : "Room/search : Bad request, from/to location not given"
-      }))
+      res.status("400").json({
+        error : "Room/search : Bad request"
+      })
     }
     // if ( req.body.roomName && ( req.body.fromName || req.body.toName || req.body.startDate )) {
     //   rres.status("400").send(JSON.stringify({
@@ -238,10 +273,9 @@ module.exports = (mongo) => {
         fromLocationID = await fromLocation._id;
         toLocationID = await toLocation._id;
       } else {
-        res.status("404").send(JSON.stringify({
-          error: true,
-          message: "Room/search : No corresponding location"
-        }))
+        res.status("404").json({
+          error: "Room/search : No corresponding location"
+        })
       }
       
       let rooms;
@@ -261,30 +295,12 @@ module.exports = (mongo) => {
       }
       // date form 2012-04-23T18:25:43.511Z
       
-      res.status("200").send(JSON.stringify({
-        error: false,
-        message : "Room/search : search successful",
-        data: rooms
-      }))
-      // if ( fromLocationID && toLocationID ){
-      //   let rooms = await mongo.roomModel.find({
-      //     from : fromLocationID,
-      //     to : toLocationID
-      //   })
-      //   res.status("200").send(JSON.stringify(rooms))
-      // }
-
-      // res.status("404").send(JSON.stringify({
-      //   error : true,
-      //   message : "Room/search : No corresponding room"
-      // }))
-    // }
+      res.send(rooms)
     } catch (error) {
       console.log(error);
-      res.status("500").send(JSON.stringify({
-        error : true,
-        message : "Room/search : Internal server error"
-      }))
+      res.status("500").json({
+        error : "Room/search : Internal server error"
+      })
     }
   })
   
