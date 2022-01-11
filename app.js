73d82b705da7a40fe053804421957aa7ec9f916b
withsang <jsa5115@naver.com>
2022-01-11 23:33:28 +0900

    Reimplement socketio server without JWTs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const proxy = require("http-proxy-middleware").createProxyMiddleware;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const expressSession = require("express-session");
+const MongoStore = require("connect-mongo");
 const fs = require("fs");
 const cors = require("cors");
 const startSocketServer = require("./src/modules/socket");
@@ -23,11 +24,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({ origin: true, credentials: true }));
 
-// 세션 및 쿠키
+// 세션 및 쿠키. 세션은 mongodb 데이터베이스에 저장합니다.
 const session = expressSession({
   secret: security.session,
   resave: true,
   saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: security.mongo,
+  }),
 });
 app.use(session);
 app.use(cookieParser());
@@ -44,4 +48,4 @@ const serverHttp = http.createServer(app).listen(security.nodePort, () => {
   console.log(`Express 서버가 ${security.nodePort}번 포트에서 시작됨.`);
 });
 
-startSocketServer(serverHttp);
+startSocketServer(serverHttp, session);
