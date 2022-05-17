265ddc3972722c87acd192f6c3eadac378a6e703
withsang <jsa5115@naver.com>
2022-05-18 02:34:09 +0900

    Add: allow admin page only for admins

--- src/middleware/adminAuth.js
+++ src/middleware/adminAuth.js
@@ -1,19 +1,24 @@
 // 관리자 유무를 확인하기 위한 미들웨어입니다.
 
-const { isLogin } = require("../auth/login");
+const { isLogin, getLoginInfo } = require("../auth/login");
 const { frontUrl } = require("../../security");
+const { userModel } = require("../db/mongo");
 
-const adminAuthMiddleware = (req, res, next) => {
+const adminAuthMiddleware = async (req, res, next) => {
   // Check if user is logged in
   if (!isLogin(req)) {
     res.redirect(frontUrl);
     return;
   }
-
-  // TODO: check if the user is admin.
-  if (true) {
-    next();
-  } else {
+  try {
+    const { id } = getLoginInfo(req);
+    const user = await userModel.findOne({ id });
+    if (user.isAdmin) {
+      next();
+    } else {
+      res.redirect(frontUrl);
+    }
+  } catch (e) {
     res.redirect(frontUrl);
   }
 };
