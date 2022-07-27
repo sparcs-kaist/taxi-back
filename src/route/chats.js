5ac5a0be427325193d110613b8cce2fdedbd3410
withsang <jsa5115@naver.com>
2022-07-27 22:43:14 +0900

    Fix: update chatHandler with main branch's progress

--- src/route/chats.js
+++ src/route/chats.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
+
 const chatHandler = require("../service/chats");
 
-router.get("/:roomId", chatHandler.roomIdHandler);
+router.get("/:roomId", chatHandler);
 
 module.exports = router;
