de5533f3606d9bb45702612f7a3e1ce60894aedc
withsang <jsa5115@naver.com>
2022-08-02 01:59:16 +0900

    Refactor: change name of access logging function

--- src/modules/apiAccessLog.js
+++ src/modules/apiAccessLog.js
@@ -1,15 +1,13 @@
 const logger = require("./logger");
 
-const apiAccessLogMiddleware = (req, res, time) => {
-  res.on("finish", () => {
-    const { method, originalUrl } = req;
-    const { statusCode } = res;
-    const userId = req.session?.loginInfo?.id || "anonymous";
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    logger.info(
-      `${userId}(${ip}) "${method} ${originalUrl}" ${statusCode} on ${time}ms`
-    );
-  });
+const logAPIAccess = (req, res, time) => {
+  const { method, originalUrl } = req;
+  const { statusCode } = res;
+  const userId = req.session?.loginInfo?.id || "anonymous";
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  logger.info(
+    `${userId}(${ip}) "${method} ${originalUrl}" ${statusCode} on ${time}ms`
+  );
 };
 
-module.exports = apiAccessLogMiddleware;
+module.exports = logAPIAccess;
