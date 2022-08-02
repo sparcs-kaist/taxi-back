a86099f6282c74cb10349b16e1921afe2dd15a79
14Kgun <geon6757@kaist.ac.kr>
2022-08-03 00:23:48 +0900

    Fix: typeerror on redis

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -8,8 +8,11 @@ const logger = require("../modules/logger");
 // 환경변수 REDIS_PATH 유무에 따라 session 저장 방식이 변경됩니다.
 let sessionStore = null;
 if (security.redis) {
-  const client = redis.createClient({ url: security.redis });
-  await client.connect().catch(logger.error);
+  const client = redis.createClient({
+    url: security.redis,
+    legacyMode: true,
+  });
+  client.connect().catch(logger.error);
   sessionStore = new RedisStore({ client });
 } else {
   const mongoUrl = security.mongo;
