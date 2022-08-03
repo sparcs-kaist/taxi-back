3c4f62f161f5b79e922cd6ddd8ee8aae09c99dd9
withsang <jsa5115@naver.com>
2022-08-04 01:08:59 +0900

    Add: Create testcases for /json/logininfo

--- test/logininfo.js
+++ test/logininfo.js
@@ -16,3 +16,53 @@ describe("logininfo handler", () => {
     logininfoHandlers.logininfoHandler(req, res);
   });
 });
+
+describe("logininfo request for logged user", () => {
+  it("should return {id: 'hello-id', sid: 'hello-sid', 'name': 'hello-name'} when user is logged in", () => {
+    const req = {
+      session: {
+        loginInfo: {
+          id: "hello-id",
+          sid: "hello-sid",
+          name: "hello-name",
+          time: Date.now(),
+        },
+      },
+    };
+    const res = {
+      json: (data) => {
+        expect(data).to.deep.equal({
+          id: "hello-id",
+          sid: "hello-sid",
+          name: "hello-name",
+        });
+      },
+    };
+    logininfoHandlers.logininfoHandler(req, res);
+  });
+});
+
+describe("logininfo request for expired session", () => {
+  it("should return {id: undefined, sid: undefined, name: undefined } when the session is expired", () => {
+    const req = {
+      session: {
+        loginInfo: {
+          id: "hello-id",
+          sid: "hello-sid",
+          name: "hello-name",
+          time: new Date(Date.now() - (3600 * 1000 + 1)).getTime(), // the session should expire after 1 hour
+        },
+      },
+    };
+    const res = {
+      json: (data) => {
+        expect(data).to.deep.equal({
+          id: undefined,
+          sid: undefined,
+          name: undefined,
+        });
+      },
+    };
+    logininfoHandlers.logininfoHandler(req, res);
+  });
+});
