c4d75d5235d4d5503bcd81e41c5a76a6927011c7
withsang <jsa5115@naver.com>
2022-08-15 04:54:43 +0900

    Add: Create log files for unhandled exceptions

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -2,40 +2,51 @@ const { createLogger, format, transports } = require("winston");
 const dailyRotateFileTransport = require("winston-daily-rotate-file");
 const path = require("path");
 
-// Define custom format for the logs
-const customFormat = format.printf(({ level, message, timestamp }) => {
-  return `${timestamp} [${level}]: ${message}`;
-});
+// 로깅에 사용하기 위한 포맷을 추가로 정의합니다.
+const customFormat = {
+  time: "YYYY-MM-DD HH:mm:ss", // 로깅 시각
+  line: format.printf(({ level, message, timestamp, stack }) => {
+    return `${timestamp} [${level}]: ${message} ${
+      level === "error" ? stack : ""
+    }`;
+  }), // 메시지 포맷
+  fileDate: "YYYY-MM-DD-HH-mm", // 파일명에 포함되는 시각
+};
 
-const datePattern = "YYYY-MM-DD-HH-mm";
-
-// Create a new logger
+/**
+ * console.log 대신 사용되는 winston Logger 객체입니다.
+ *
+ * 전체 로그는 *.combined.log 파일에, 예외 처리로 핸들링 된 오류 로그는 *.error.log 파일에, 예외 처리가 되지 않은 오류는 *.unhandled.log에 저장됩니다.
+ * @property {function} info() - 일반적인 정보 기록을 위한 로깅(API 접근 기록 등)을 위해 사용합니다.
+ * @property {function} error() - 오류 메시지를 로깅하기 위해 사용합니다.
+ */
 const logger = createLogger({
-  level: "info",
   format: format.combine(
-    format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss",
-    }),
+    format.timestamp({ format: customFormat.time }),
     format.errors({ stack: true }),
     format.splat(),
     format.json(),
-    customFormat
+    customFormat.line
   ),
   defaultMeta: { service: "sparcs-taxi" },
   transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
     new dailyRotateFileTransport({
-      filename: path.resolve("logs/error-%DATE%.log"),
-      datePattern,
+      filename: path.resolve("logs/%DATE%-combined.log"),
+      datePattern: customFormat.fileDate,
+      maxsize: 5242880, // 5MB
+      level: "info",
+    }),
+    new dailyRotateFileTransport({
+      filename: path.resolve("logs/%DATE%-error.log"),
+      datePattern: customFormat.fileDate,
       maxsize: 5242880, // 5MB
       level: "error",
     }),
+  ],
+  exceptionHandlers: [
     new dailyRotateFileTransport({
-      filename: path.resolve("logs/combined-%DATE%.log"),
-      datePattern,
+      filename: path.resolve("logs/%DATE%-unhandled.log"),
+      datePattern: customFormat.fileDate,
       maxsize: 5242880, // 5MB
     }),
   ],
@@ -45,7 +56,13 @@ const logger = createLogger({
 if (process.env.NODE_ENV !== "production" || process.env.NODE_ENV !== "test") {
   logger.add(
     new transports.Console({
-      format: format.combine(format.colorize(), format.simple(), customFormat),
+      format: format.combine(
+        format.timestamp({ format: customFormat.time }),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.colorize(),
+        customFormat.line
+      ),
     })
   );
 }
