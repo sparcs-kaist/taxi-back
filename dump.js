8d23e3cc935a5e68e7713e851c613f005c5bf980
withsang <jsa5115@naver.com>
2022-08-19 23:50:45 +0900

    Add: separate Socket.io URL environment variable

--- dump.js
+++ dump.js
@@ -4,12 +4,13 @@ const security = require("./security");
 
 const main = async () => {
   const { stdout, stderr } = await exec(`mongodump ${security.mongo}`);
+  console.log("dump 디렉토리에 데이터베이스 데이터를 덤프했습니다.");
   process.exit(0);
 };
 
 try {
   main();
-} catch (_) {
+} catch {
   console.log(
     "DB 연결 주소가 올바르지 않습니다. DB 연결 주소를 다시 한 번 확인해주세요."
   );
--- restore.js
+++ restore.js
@@ -7,12 +7,15 @@ const main = async () => {
   const { stdout, stderr } = await exec(
     `mongorestore ${security.mongo} dump/${dbName}`
   );
+  console.log(
+    "dump 디렉토리로부터 데이터베이스 정보를 성공적으로 복원했습니다."
+  );
   process.exit(0);
 };
 
 try {
   main();
-} catch (_) {
+} catch {
   console.log(
     "DB를 덤프해올 디렉토리가 존재하지 않습니다. 경로를 다시 한 번 확인해주세요."
   );
