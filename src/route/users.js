37c94a3fe0a698c08b9fdf4448129e9f27e24131
seowan <ptptsw34@gmail.com>
2022-09-20 01:01:08 +0900

    Add: add report

--- src/route/users.js
+++ src/route/users.js
@@ -38,4 +38,6 @@ router.post(
 // 프로필 이미지가 S3에 정상적으로 업로드가 되었는지 확인합니다.
 router.get("/editProfileImg/done", userHandlers.editProfileImgDoneHandler);
 
+router.post("/report", userHandlers.reportHandler);
+
 module.exports = router;
