af3f7a483c00cc238409577d2520f7305ff81eb1
happycastle <41810556+happycastle114@users.noreply.github.com>
2022-11-01 20:22:46 +0900

    Refactor: jwt functions

--- src/modules/jwt.js
+++ src/modules/jwt.js
@@ -5,37 +5,41 @@ const option = require('../config/secretKey').option;
 const TOKEN_EXPIRED = -3;
 const TOKEN_INVALID = -2;
 
-module.exports = {
-    sign: async ({id, type}) => {
-        const payload = {
-            id: id,
-            type: type
-        };
-        if (type === 'refresh') {
-            option.expiresIn = '30d';
-        }
-        if (type === 'access') {
-            option.expiresIn = '7d';
-        }
-        
-        const result = {
-            token: jwt.sign(payload, secretKey, option),
-        };
-        return result;
-        },
-    verify: async (token) => {
-        let decoded;
-        try {
-            decoded = jwt.verify(token, secretKey);
-        } catch (err) {
-            if (err.message === 'jwt expired') {
-                return TOKEN_EXPIRED;
-            } else if (err.message === 'invalid token') {
-                return TOKEN_INVALID;
-            } else {
-                return TOKEN_INVALID;
-            }
-        }
-        return decoded;
+const signJwt = async ({id, type}) => {
+    const payload = {
+        id: id,
+        type: type
+    };
+    if (type === 'refresh') {
+        option.expiresIn = '30d';
     }
+    if (type === 'access') {
+        option.expiresIn = '7d';
+    }
+    
+    const result = {
+        token: jwt.sign(payload, secretKey, option),
+    };
+    return result;
+};
+
+const verifyJwt = async (token) => {
+    let decoded;
+    try {
+        decoded = jwt.verify(token, secretKey);
+    } catch (err) {
+        if (err.message === 'jwt expired') {
+            return TOKEN_EXPIRED;
+        } else if (err.message === 'invalid token') {
+            return TOKEN_INVALID;
+        } else {
+            return TOKEN_INVALID;
+        }
+    }
+    return decoded;
+};
+
+module.exports = {
+    sign: signJwt,
+    verify: verifyJwt
 }
