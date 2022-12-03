d217bd73810ad16e8a8e80952f264b979ebc8726
chlehdwon <cookie000215@gmail.com>
2022-12-03 23:17:02 +0900

    Refactor: add testRemover and remove status checkings

--- test/rooms.js
+++ test/rooms.js
@@ -2,14 +2,16 @@ const expect = require("chai").expect;
 const express = require("express");
 const roomsHandlers = require("../src/service/rooms");
 const { userModel, roomModel, locationModel } = require("../src/db/mongo");
-const { userGenerator } = require("./utils");
+const { userGenerator, testRemover } = require("./utils");
 const app = express();
+
+let testData = { rooms: [], users: [], chat: [], location: [], report: [] };
 
 // rooms.js 관련 8개의 handler을 테스트
 // 1. test1이 1분 뒤에 출발하는 test-room 방을 생성
 describe("[rooms] 1.createHandler", () => {
   it("should create room which departs after 1 minute", async () => {
-    const testUser1 = await userGenerator("test1");
+    const testUser1 = await userGenerator("test1", testData);
     const testFrom = await locationModel.findOne({ koName: "대전역" });
     const testTo = await locationModel.findOne({ koName: "택시승강장" });
     const req = {
@@ -26,9 +28,6 @@ describe("[rooms] 1.createHandler", () => {
     const res = {
       send: (data) => {
         expect(data).to.has.property("name", "test-room");
-      },
-      status: (data) => {
-        expect(data).to.equal(200);
       },
     };
 
@@ -50,9 +49,6 @@ describe("[rooms] 2.infoHandler", () => {
         expect(data).to.has.property("name", "test-room");
         expect(data).to.has.property("isOver");
       },
-      status: (data) => {
-        expect(data).to.equal(200);
-      },
     };
 
     await roomsHandlers.infoHandler(req, res);
@@ -62,8 +58,9 @@ describe("[rooms] 2.infoHandler", () => {
 // 3. test2가 test-room에 join
 describe("[rooms] 3.joinHandler", () => {
   it("should return information of room and join", async () => {
-    const testUser2 = await userGenerator("test2");
+    const testUser2 = await userGenerator("test2", testData);
     const testRoom = await roomModel.findOne({ name: "test-room" });
+    testData["rooms"].push(testRoom);
     const req = {
       body: {
         roomId: testRoom._id,
@@ -75,9 +72,6 @@ describe("[rooms] 3.joinHandler", () => {
       send: (data) => {
         expect(data).to.has.property("name", "test-room");
         expect(data.part).to.have.lengthOf(2);
-      },
-      status: (data) => {
-        expect(data).to.equal(200);
       },
     };
 
@@ -105,9 +99,6 @@ describe("[rooms] 4.searchHandler", () => {
         expect(data[0]).to.has.property("name", "test-room");
         expect(data[0]).to.has.property("settlementTotal");
       },
-      status: (data) => {
-        expect(data).to.equal(200);
-      },
     };
 
     await roomsHandlers.searchHandler(req, res);
@@ -123,11 +114,8 @@ describe("[rooms] 5.searchByUserHandler", () => {
     };
     const res = {
       json: (data) => {
-        expect(data).to.has.property("ongoing");
-        expect(data).to.has.property("done");
-      },
-      status: (data) => {
-        expect(data).to.equal(200);
+        expect(data["ongoing"][0]).to.has.property("name", "test-room");
+        expect(data["done"][0]).to.be.an("undefined");
       },
     };
 
@@ -151,9 +139,6 @@ describe("[rooms] 6.commitPaymentHandler", () => {
         expect(data).to.has.property("isOver", true);
         expect(data).to.has.property("settlementTotal", 1);
       },
-      status: (data) => {
-        expect(data).to.equal(200);
-      },
     };
     await roomsHandlers.commitPaymentHandler(req, res);
   });
@@ -174,9 +159,6 @@ describe("[rooms] 7.settlementHandler", () => {
         expect(data).to.has.property("isOver", true);
         expect(data).to.has.property("settlementTotal", 2);
       },
-      status: (data) => {
-        expect(data).to.equal(200);
-      },
     };
     await roomsHandlers.settlementHandler(req, res);
   });
@@ -186,9 +168,7 @@ describe("[rooms] 7.settlementHandler", () => {
 describe("[rooms] 8.abortHandler", () => {
   const removeTestData = async () => {
     // drop all testData
-    await roomModel.deleteOne({ name: "test-room" });
-    await userModel.deleteOne({ id: "test1" });
-    await userModel.deleteOne({ id: "test2" });
+    await testRemover(testData);
   };
   it("should return information of room and abort user", async () => {
     const testUser2 = await userModel.findOne({ id: "test2" });
@@ -203,9 +183,6 @@ describe("[rooms] 8.abortHandler", () => {
       send: (data) => {
         expect(data).to.has.property("name", "test-room");
         expect(data.part).to.have.lengthOf(1);
-      },
-      status: (data) => {
-        expect(data).to.equal(200);
       },
     };
     await roomsHandlers.abortHandler(req, res);
--- test/utils.js
+++ test/utils.js
@@ -1,8 +1,14 @@
-const { userModel } = require("../src/db/mongo");
+const {
+  userModel,
+  roomModel,
+  chatModel,
+  locationModel,
+  reportModel,
+} = require("../src/db/mongo");
 const { generateProfileImageUrl } = require("../src/modules/modifyProfile");
 
 // 테스트를 위한 유저 생성 함수
-const userGenerator = async (username) => {
+const userGenerator = async (username, testData) => {
   const testUser = new userModel({
     id: username,
     name: username + "-name",
@@ -18,7 +24,30 @@ const userGenerator = async (username) => {
     email: username + ".kaist.ac.kr",
   });
   await testUser.save();
+  testData["users"].push(testUser);
   return testUser;
 };
 
-module.exports = { userGenerator };
+const testRemover = async (testData) => {
+  for (const roomData of testData["rooms"]) {
+    await roomModel.deleteOne({ _id: roomData });
+  }
+
+  for (const userData of testData["users"]) {
+    await userModel.deleteOne({ _id: userData });
+  }
+
+  for (const chatData of testData["chat"]) {
+    await chatModel.deleteOne({ _id: chatData._id });
+  }
+
+  for (const locationData of testData["location"]) {
+    await locationModel.deleteOne({ _id: locationData._id });
+  }
+
+  for (const reportData of testData["report"]) {
+    await reportModel.deleteOne({ _id: reportData._id });
+  }
+};
+
+module.exports = { userGenerator, testRemover };
