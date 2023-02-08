568f5c454beaad5ce1b2a8c1aaa1a5e2ea349322
chlehdwon <cookie000215@gmail.com>
2023-02-08 13:25:54 +0000

    Refactor: locations.js

--- test/locations.js
+++ test/locations.js
@@ -1,14 +1,13 @@
 const expect = require("chai").expect;
 const locationHandlers = require("../src/service/locations");
+const httpMocks = require("node-mocks-http");
 
 describe("[locations] 1.getAllLocationsHandler", () => {
   it("should return information of locations correctly", async () => {
-    const req = {};
-    const res = {
-      json: (data) => {
-        expect(data.locations).not.to.have.lengthOf(0);
-      },
-    };
+    let req = httpMocks.createRequest({});
+    let res = httpMocks.createResponse();
     await locationHandlers.getAllLocationsHandler(req, res);
+
+    expect(res._getJSONData().locations).not.to.have.lengthOf(0);
   });
 });
