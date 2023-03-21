8e11f25e27a02b209dae82f90f297ebe03c99ae4
chlehdwon <cookie000215@gmail.com>
2023-03-21 13:03:24 +0000

    Docs: integrate swagger docs

--- src/route/docs.js
+++ src/route/docs.js
@@ -1,35 +1,7 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
-const swaggereJsdoc = require("swagger-jsdoc");
-
+const swaggerSpec = require("../route/docs/docs.json");
 const router = express.Router();
-const swaggerSpec = swaggereJsdoc({
-  definition: {
-    openapi: "3.0.3",
-    info: {
-      title: "Taxi API Document",
-      version: "1.0.0",
-    },
-    basePath: "/",
-    tags: [
-      {
-        name: "locations",
-        description: "출발지/도착지 정보 제공",
-      },
-      {
-        name: "logininfo",
-        description: "로그인 정보 제공",
-      },
-      {
-        name: "reports",
-        description: "사용자 신고 및 신고 기록 조회",
-      },
-    ],
-    consumes: ["application/json"],
-    produces: ["application/json"],
-  },
-  apis: ["src/route/*.js"],
-});
 
 router.use(swaggerUi.serve);
 router.use(swaggerUi.setup(swaggerSpec, { explorer: true }));
--- src/route/reports.js
+++ src/route/reports.js
@@ -7,56 +7,6 @@ const reportHandlers = require("../service/reports");
 
 // 라우터 접근 시 로그인 필요
 router.use(require("../middleware/auth"));
-/**
- * @swagger
- * /create:
- *   post:
- *     tags: [reports]
- *     summary: 신고 작성
- *     description: 주어진 유저를 전달된 사유로 신고함
- *     requestBody:
- *       description: Update an existent user in the store
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/createHandler'
- *     responses:
- *       200:
- *         description: report successful
- *         content:
- *           text/plain:
- *            schema:
- *              type: string
- *              example: report successful
- *       500:
- *         description: internal server error
- *         content:
- *           text/plain:
- *            schema:
- *              type: string
- *              example: internal server error
- * components:
- *   schemas:
- *     createHandler:
- *       type: object
- *       required:
- *         - reportedId
- *         - type
- *         - time
- *       properties:
- *         reportedId:
- *           type: string
- *           pattern: '^[a-fA-F\d]{24}$'
- *         type:
- *           type: string
- *           enum: ["no-settlement", "no-show", "etc-reason"]
- *         etcDetail:
- *           type: string
- *           maxLength: 30
- *         time:
- *           type: string
- *           format: date-time
- */
 router.post(
   "/create",
   [
@@ -69,40 +19,6 @@ router.post(
   reportHandlers.createHandler
 );
 
-/**
- * @swagger
- * /searchByUser:
- *   get:
- *     tags: [reports]
- *     summary: 신고 내역 반환
- *     description: 로그인된 사용자의 신고한 내역과, 신고받은 내역을 반환한다 <br/>
- *                  1000개의 limit이 있다.
- *     responses:
- *       200:
- *         description: 신고된 내역과 신고 받은 내역
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 reporting:
- *                   type: array
- *                   items:
- *                     type: object
- *                     description: Report
- *                 reported:
- *                   type: array
- *                   items:
- *                     type: object
- *                     description: Report
- *       500:
- *         description: "internal server error"
- *         content:
- *           text/plain:
- *             schema:
- *               type: string
- *               example: "internal server error"
- */
 router.get("/searchByUser", reportHandlers.searchByUserHandler);
 
 module.exports = router;
