84049806cdd9e9d46e9214428c8eaec68a056bd2
chlehdwon <cookie000215@gmail.com>
2023-03-28 13:43:04 +0000

    Refactor: use JSON.parse

--- loadenv.js
+++ loadenv.js
@@ -33,5 +33,5 @@ module.exports = {
   googleApplicationCredentials:
     process.env.GOOGLE_APPLICATION_CREDENTIALS &&
     JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS),
-  testAccounts: process.env.TEST_ACCOUNTS.split(","),
+  testAccounts: JSON.parse(process.env.TEST_ACCOUNTS),
 };
