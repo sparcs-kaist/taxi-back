181367a2de22ef9424733e08a62f1c2d799e0599
14Kgun <geon6757@kaist.ac.kr>
2023-04-05 02:45:04 +0900

    Move: "/search"

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -6,6 +6,22 @@ const roomHandlers = require("../services/rooms");
 const validator = require("../middlewares/validator");
 const patterns = require("../modules/patterns");
 
+// 조건(이름, 출발지, 도착지, 날짜)에 맞는 방들을 모두 반환한다.
+router.get(
+  "/search",
+  [
+    query("name").optional().matches(patterns.room.name),
+    query("from").optional().isMongoId(),
+    query("to").optional().isMongoId(),
+    query("time").optional().isISO8601(),
+    query("withTime").toBoolean().optional().isBoolean(),
+    query("maxPartLength").optional().isInt({ min: 2, max: 4 }),
+    query("isHome").toBoolean().isBoolean(),
+  ],
+  validator,
+  roomHandlers.searchHandler
+);
+
 // 라우터 접근 시 로그인 필요
 router.use(require("../middlewares/auth"));
 
@@ -49,22 +65,6 @@ router.post(
   body("roomId").isMongoId(),
   validator,
   roomHandlers.abortHandler
-);
-
-// 조건(이름, 출발지, 도착지, 날짜)에 맞는 방들을 모두 반환한다.
-router.get(
-  "/search",
-  [
-    query("name").optional().matches(patterns.room.name),
-    query("from").optional().isMongoId(),
-    query("to").optional().isMongoId(),
-    query("time").optional().isISO8601(),
-    query("withTime").toBoolean().optional().isBoolean(),
-    query("maxPartLength").optional().isInt({ min: 2, max: 4 }),
-    query("isHome").toBoolean().isBoolean(),
-  ],
-  validator,
-  roomHandlers.searchHandler
 );
 
 // 로그인된 사용자의 모든 방들을 반환한다.
