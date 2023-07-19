81b7531ca10d0505df59db1c3ab10d08e1485e76
Hyogyeong8 <khg4773@gmail.com>
2023-07-19 21:19:39 +0900

    add: contents

--- src/views/email.js
+++ src/views/email.js
@@ -1,23 +1,3 @@
-const styleTitle: CSS = {
-  display: "flex",
-  alignItems: "center",
-  ...theme.font14,
-  color: theme.gray_text,
-  whiteSpace: "nowrap",
-  marginTop: "10px",
-};
-const styleNickname: CSS = {
-  width: "100%",
-  ...theme.font14,
-  border: "none",
-  outline: "none",
-  borderRadius: "6px",
-  padding: "6px 12px",
-  marginLeft: "10px",
-  background: theme.purple_light,
-  boxShadow: theme.shadow_purple_input_inset,
-};
-
 const emailView = (title, contents) => `
   <style>
     .title {
