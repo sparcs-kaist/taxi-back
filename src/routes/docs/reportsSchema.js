fb30ceaa84b128c051440930b43e8c79605fb13d
chlehdwon <cookie000215@gmail.com>
2023-08-01 13:00:42 +0000

    Add: createHandler validation schema

--- src/routes/docs/reportsSchema.js
+++ src/routes/docs/reportsSchema.js
@@ -6,18 +6,26 @@ const reportsSchema = {
       reportedId: {
         type: "string",
         pattern: "^[a-fA-F\\d]{24}$",
+        required: true,
       },
       type: {
         type: "string",
         enum: ["no-settlement", "no-show", "etc-reason"],
+        required: true,
       },
       etcDetail: {
         type: "string",
         maxLength: 30,
+        default: "",
       },
       time: {
         type: "string",
         format: "date-time",
+        required: true,
+      },
+      roomId: {
+        type: "string",
+        pattern: "^[a-fA-F\\d]{24}$",
       },
     },
     errorMessage: "validation: bad request",
