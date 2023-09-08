2191d12a1c41e63bd06bb0f34abec70b00c2de29
withsang <jsa5115@naver.com>
2023-09-09 05:42:11 +0900

    Docs: add event/items docs

---
+++ src/lottery/routes/docs/swaggerDocs.js
@@ -0,0 +1,22 @@
+const { eventMode } = require("../../../../loadenv");
+const itemsDocs = require("./items");
+
+const apiPrefix = `/events/${eventMode}`;
+
+const eventSwaggerDocs = {
+  tags: [
+    {
+      // TODO: edit
+      name: `${apiPrefix}/items`,
+      description: "이벤트 - 아이템 관련 API",
+    },
+  ],
+  paths: {
+    ...itemsDocs,
+  },
+  components: {
+    schemas: {},
+  },
+};
+
+module.exports = eventSwaggerDocs;
