5436750250b73433c0c5ba407a4d82342e6c3e97
withsang <jsa5115@naver.com>
2023-09-17 03:10:49 +0900

    Refactor: remove mongodb from docker compose

--- worker.js
+++ worker.js
@@ -3,12 +3,12 @@ const logger = require("./src/modules/logger");
 // BullMQ worker들을 등록합니다.
 const workers = [require("./src/workers/sendNotification")];
 
-// 오류 발생 시 안전하게 로깅합니다.
+// 오류 발생 시 로깅합니다.
 process.on("uncaughtException", function (err) {
   logger.error(`핸들링되지 않은 예외 발생: ${err}`);
 });
 
-// 오류 발생 시 안전하게 로깅합니다.
+// 오류 발생 시 로깅합니다.
 process.on("unhandledRejection", (err) => {
   logger.error(`Promise에서 핸들링되지 않은 rejection 발생: ${err}`);
 });
