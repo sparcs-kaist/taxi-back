4a258ffebf333ffd81b63526c640a75d16dfbe23
static <kmc7468@naver.com>
2023-09-18 19:37:22 +0900

    Add: add validator to /events/2023fall/items/purchase/:itemId endpoint

--- src/lottery/routes/items.js
+++ src/lottery/routes/items.js
@@ -4,7 +4,16 @@ const router = express.Router();
 const itemsHandlers = require("../services/items");
 const auth = require("../../middlewares/auth");
 
+const { param } = require("express-validator");
+const validator = require("../../middlewares/validator");
+
 router.get("/list", itemsHandlers.listHandler);
-router.post("/purchase/:itemId", auth, itemsHandlers.purchaseHandler);
+router.post(
+  "/purchase/:itemId",
+  auth,
+  param("itemId").isMongoId(),
+  validator,
+  itemsHandlers.purchaseHandler
+);
 
 module.exports = router;
