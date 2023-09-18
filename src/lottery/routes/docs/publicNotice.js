f5fb681f5fb5812459421bcafc1c05e62b7640bf
TaehyeonPark <devtaehyeon@gmail.com>
2023-09-18 19:57:47 +0900

    ADD: Merging process...

--- src/lottery/routes/docs/publicNotice.js
+++ src/lottery/routes/docs/publicNotice.js
@@ -2,28 +2,6 @@ const { eventMode } = require("../../../../loadenv");
 const apiPrefix = `/events/${eventMode}/public-notice`;
 
 const publicNoticeDocs = {};
-<<<<<<< HEAD
-publicNoticeDocs[`${apiPrefix}/get-recent-transaction`] = {
-  get: {
-    tags: [`${apiPrefix}`],
-    summary: "공지를 띄움",
-    description: "상품 구매, 뽑기 획득 등등에 대한 공지를 반환",
-    responses: {
-      200: {
-        content: {
-          "application/json": {
-            schema: {
-              type: "array",
-              description:
-                "use 트랜젝션의 정보를 담은 string 배열. 구매자의 익명 이름과 구매 내역을 문자열로 변환 후 이를 배열로 담아 5개 반환",
-              example: [
-                "su****님께서 매점2000원권을(를) 구입하셨습니다.",
-                "mo****님께서 치킨을(를) 구입하셨습니다.",
-                "tu*****님께서 매점2000원권을(를) 구입하셨습니다.",
-                "as**님께서 진짜송편을(를) 구입하셨습니다.",
-                "qw**님께서 전동킥보드을(를) 뽑았습니다.",
-              ],
-=======
 publicNoticeDocs[`${apiPrefix}/leaderboard`] = {
   get: {
     tags: [`${apiPrefix}`],
@@ -91,7 +69,6 @@ publicNoticeDocs[`${apiPrefix}/leaderboard`] = {
                   example: 0.00003,
                 },
               },
->>>>>>> dev
             },
           },
         },
