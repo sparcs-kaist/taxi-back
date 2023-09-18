c50556153f89d1fe5861ef853e9243a0697e8e5d
TaehyeonPark <devtaehyeon@gmail.com>
2023-09-18 20:13:53 +0900

    ADD: Mergin process...

--- src/lottery/services/events.js
+++ src/lottery/services/events.js
@@ -4,11 +4,11 @@ const contracts = require("../modules/contracts/2023fall");
 // 인스타그램 스토리에 이벤트를 공유했을 때.
 const instagramEventShareHandler = async (req, res) => {
   try {
-    const contractsResult = contracts.completeEventSharingOnInstagramQuest();
+    const contractsResult = contracts.completeEventSharingOnInstagramQuest(req.userOid);
     if (contractsResult) {
       res.json({ contractsResult });
     } else if (contractsResult === null) {
-      res.json({});
+      res.json({ contractsResult });
     } else {
       logger.error(err);
       res.json({ error: "Events/Insagram/Share-Event" });
@@ -22,11 +22,11 @@ const instagramEventShareHandler = async (req, res) => {
 // 인스타그램 스토리에 아이템 구매 내역을 공유했을 때.
 const instagramPurchaseShareHandler = async (req, res) => {
   try {
-    const contractsResult = contracts.completePurchaseSharingOnInstagramQuest();
+    const contractsResult = contracts.completePurchaseSharingOnInstagramQuest(req.userOid);
     if (contractsResult) {
       res.json({ contractsResult });
     } else if (contractsResult === null) {
-      res.json({});
+      res.json({ contractsResult });
     } else {
       logger.error(err);
       return { error: "Events/Insagram/Share-Purchase" };
