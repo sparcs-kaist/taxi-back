450a4ceb57d9edf675f0828b5126cb1dd0e9e016
cokia <hanu@hanukoon.com>
2023-09-20 02:14:54 +0900

    fix: collect phonenumber when event agree

--- src/lottery/routes/globalState.js
+++ src/lottery/routes/globalState.js
@@ -2,12 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 const globalStateHandlers = require("../services/globalState");
+const { validateBody } = require("../../middlewares/ajv");
 
 router.get("/", globalStateHandlers.getUserGlobalStateHandler);
 
 // 아래의 Endpoint 접근 시 로그인 필요
 router.use(require("../../middlewares/auth"));
 
-router.post("/create", globalStateHandlers.createUserGlobalStateHandler);
+router.post("/create",
+  validateBody({phoneNumber: {required: true, type: "string", pattern: "/^010-?([0-9]{3,4})-?([0-9]{4})$/"}}),
+  globalStateHandlers.createUserGlobalStateHandler);
 
 module.exports = router;
