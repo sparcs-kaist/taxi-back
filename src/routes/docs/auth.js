065827e1b8af510c76a2168957b22213edf2e8ce
Dongwon Choi <cookie000215@gmail.com>
2023-09-19 15:13:56 +0000

    Docs: auth.js basic format done

--- src/routes/docs/auth.js
+++ src/routes/docs/auth.js
@@ -25,9 +25,16 @@ const authDocs = {
           description: "앱인지 여부",
         },
       ],
-      response: {
+      responses: {
         302: {
           description: "SPARCS SSO 로그인 페이지로 리다이렉트",
+          headers: {
+            Location: {
+              type: "string",
+              description: "SPARCS SSO 로그인 페이지",
+              format: "uri",
+            },
+          },
         },
       },
     },
@@ -56,11 +63,19 @@ const authDocs = {
           description: "login 성공 여부 확인을 위한 state",
         },
       ],
-      response: {
+      responses: {
         302: {
           description:
-            "로그인 성공 후 페이지, 혹은 로그인 실패 URI로 리다이렉트",
+            "로그인 성공 후 페이지 URI로, 혹은 로그인 실패 URI로 리다이렉트",
+          headers: {
+            Location: {
+              type: "string",
+              description: "로그인 성공 후 페이지 URI, 혹은 로그인 실패 URI",
+              format: "uri",
+            },
+          },
         },
+        400: errorsDocs["400"],
       },
     },
   },
@@ -69,15 +84,27 @@ const authDocs = {
       tags: ["/auth"],
       summary: "세션 삭제 및 사용자 로그아웃",
       description: "세션 삭제 및 사용자 로그아웃",
+      parameters: [
+        {
+          in: "query",
+          name: "redirect",
+          schema: {
+            type: "string",
+          },
+          description: "로그아웃 후 리다이렉트 URI",
+        },
+      ],
       responses: {
         200: {
           content: {
-            schema: {
-              type: "object",
-              properties: {
-                ssoLogoutUrl: {
-                  type: "string",
-                  description: "SSO 로그아웃 URL",
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  ssoLogoutUrl: {
+                    type: "string",
+                    description: "SSO 로그아웃 URL",
+                  },
                 },
               },
             },
@@ -90,24 +117,173 @@ const authDocs = {
   "/auth/app/token/login": {
     get: {
       tags: ["/auth"],
+      summary: "Access token을 사용하여 로그인",
+      description: "앱에서 Access Token을 사용하여 로그인 시도",
+      parameters: [
+        {
+          in: "query",
+          name: "accessToken",
+          schema: {
+            type: "string",
+          },
+          description: "만료 되지 않은 유효한 JWT Access Token",
+        },
+        {
+          in: "query",
+          name: "deviceToken",
+          schema: {
+            type: "string",
+          },
+          description: "Device Token",
+        },
+      ],
+      responses: {
+        200: {
+          description: "성공 메세지",
+          content: {
+            "text/plain": {
+              schema: {
+                type: "string",
+                example: "success",
+              },
+            },
+          },
+        },
+        401: errorsDocs["401"],
+        500: errorsDocs["500"],
+      },
     },
   },
   "/auth/app/token/refresh": {
     get: {
       tags: ["/auth"],
+      summary: "만료된 Access Token 갱신",
+      description: "앱에서 Access Token을 Refresh Token을 활용하여 갱신",
+      parameters: [
+        {
+          in: "query",
+          name: "accessToken",
+          schema: {
+            type: "string",
+          },
+          description: "만료된 유효한 JWT Access Token",
+        },
+        {
+          in: "query",
+          name: "refreshToken",
+          schema: {
+            type: "string",
+          },
+          description: "만료되지 않은 유효한 JWT Refresh Token",
+        },
+      ],
+      responses: {
+        200: {
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  accessToken: {
+                    type: "string",
+                    description: "새로운 JWT Access Token",
+                  },
+                  refreshToken: {
+                    type: "string",
+                    description: "새로운 Refresh Token",
+                  },
+                },
+              },
+            },
+          },
+        },
+        400: errorsDocs["400"],
+        401: errorsDocs["401"],
+        501: errorsDocs["501"],
+      },
     },
   },
   "/auth/app/device": {
     post: {
       tags: ["/auth"],
+      summary: "기기의 Device Token을 데이터베이스에 등록",
+      description: "App 기기의 Device Token을 데이터베이스에 등록",
+      requestBody: {
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                accessToken: {
+                  type: "string",
+                  description: "만료 되지 않은 유효한 JWT Access Token",
+                },
+                deviceToken: {
+                  type: "string",
+                  description:
+                    "Firebase 라이브러리에서 제공해주는 Device Token",
+                },
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        200: {
+          description: "성공 메세지",
+          content: {
+            "text/plain": {
+              schema: {
+                type: "string",
+                exapmle: "success",
+              },
+            },
+          },
+        },
+        400: errorsDocs["400"],
+        401: errorsDocs["401"],
+        500: errorsDocs["500"],
+      },
     },
     delete: {
       tags: ["/auth"],
-    },
-  },
-  "/auth/app/token/generate": {
-    get: {
-      tags: ["/auth"],
+      summary: "기기의 Device Token을 데이터베이스에서 삭제",
+      description: "App 기기의 Device Token을 데이터베이스에 삭제",
+      requestBody: {
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                accessToken: {
+                  type: "string",
+                  description: "만료 되지 않은 유효한 JWT accessToken",
+                },
+                deviceToken: {
+                  type: "string",
+                  description: "Firebase 라이브러리에서 제공해주는 DeviceToken",
+                },
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        200: {
+          description: "성공 메세지",
+          content: {
+            "text/plain": {
+              schema: {
+                type: "string",
+                exapmle: "success",
+              },
+            },
+          },
+        },
+        400: errorsDocs["400"],
+        401: errorsDocs["401"],
+        500: errorsDocs["500"],
+      },
     },
   },
 };
--- src/routes/docs/errors.js
+++ src/routes/docs/errors.js
@@ -1,4 +1,26 @@
 const errorsDocs = {
+  400: {
+    description: "invalid request",
+    content: {
+      "text/plain": {
+        schema: {
+          type: "string",
+          example: "invalid request",
+        },
+      },
+    },
+  },
+  401: {
+    description: "invalid request",
+    content: {
+      "text/plain": {
+        schema: {
+          type: "string",
+          example: "Invalid token",
+        },
+      },
+    },
+  },
   500: {
     description: "internal server error",
     content: {
@@ -6,6 +28,17 @@ const errorsDocs = {
         schema: {
           type: "string",
           example: "internal server error",
+        },
+      },
+    },
+  },
+  501: {
+    description: "server error",
+    content: {
+      "text/plain": {
+        schema: {
+          type: "string",
+          example: "server error",
         },
       },
     },
