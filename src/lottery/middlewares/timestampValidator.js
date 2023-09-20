7d54a704b9fee3f3a4b04cd40a62788631c97102
static <kmc7468@naver.com>
2023-09-20 23:03:11 +0900

    Fix: check if eventPeriod is defined in timestampValidator

--- src/lottery/middlewares/timestampValidator.js
+++ src/lottery/middlewares/timestampValidator.js
@@ -6,6 +6,7 @@ const eventPeriod = eventConfig && {
 
 const timestampValidator = (req, res, next) => {
   if (
+    !eventPeriod ||
     req.timestamp >= eventPeriod.endAt ||
     req.timestamp < eventPeriod.startAt
   ) {
