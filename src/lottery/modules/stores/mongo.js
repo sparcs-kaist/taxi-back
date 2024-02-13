e832dbff3939c7868847a02b9fb6c28aedb33324
static <kmc7468@naver.com>
2024-02-13 14:42:53 +0900

    Add: mongoose model name prefix

--- src/lottery/modules/stores/mongo.js
+++ src/lottery/modules/stores/mongo.js
@@ -138,9 +138,24 @@ transactionSchema.set("timestamps", {
   updatedAt: false,
 });
 
+const { eventConfig } = require("../../../../loadenv");
+
+// 이벤트마다 사용된 모델을 구분하기 위해 이름에 Prefix를 붙입니다.
+// 2023년 가을학기 이벤트 때에는 Prefix를 사용하지 않았으므로, 해당 경우에는 Prefix를 붙이지 않습니다.
+const modelNamePrefix =
+  (eventConfig &&
+    (eventConfig.mode === "2023fall" ? "" : `${eventConfig.mode}/`)) ??
+  "";
+
 module.exports = {
-  eventStatusModel: mongoose.model("EventStatus", eventStatusSchema),
-  questModel: mongoose.model("Quest", questSchema),
-  itemModel: mongoose.model("Item", itemSchema),
-  transactionModel: mongoose.model("Transaction", transactionSchema),
+  eventStatusModel: mongoose.model(
+    `${modelNamePrefix}EventStatus`,
+    eventStatusSchema
+  ),
+  questModel: mongoose.model(`${modelNamePrefix}Quest`, questSchema),
+  itemModel: mongoose.model(`${modelNamePrefix}Item`, itemSchema),
+  transactionModel: mongoose.model(
+    `${modelNamePrefix}Transaction`,
+    transactionSchema
+  ),
 };
