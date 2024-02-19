833aade793bdae147954d8dbf56dae2fbf36bf41
static <kmc7468@naver.com>
2024-02-19 23:19:43 +0900

    Refactor: restore index.js

--- src/lottery/index.js
+++ src/lottery/index.js
@@ -1,49 +1,47 @@
+const express = require("express");
+const {
+  eventStatusModel,
+  questModel,
+  itemModel,
+  transactionModel,
+} = require("./modules/stores/mongo");
+
+const { buildResource } = require("../modules/adminResource");
+const {
+  addOneItemStockAction,
+  addFiveItemStockAction,
+} = require("./modules/items");
+
 const { eventConfig } = require("../../loadenv");
+const contracts = eventConfig && require("./modules/contracts");
 
-if (eventConfig) {
-  // [Routes] 기존 docs 라우터의 docs extend
-  require("./routes/docs")();
+// [Routes] 기존 docs 라우터의 docs extend
+eventConfig && require("./routes/docs")();
 
-  const express = require("express");
-  const lotteryRouter = express.Router();
+const lotteryRouter = express.Router();
 
-  // [Middleware] 모든 API 요청에 대하여 origin 검증
-  lotteryRouter.use(require("../middlewares/originValidator"));
+// [Middleware] 모든 API 요청에 대하여 origin 검증
+lotteryRouter.use(require("../middlewares/originValidator"));
 
-  // [Router] APIs
-  lotteryRouter.use("/globalState", require("./routes/globalState"));
-  lotteryRouter.use("/transactions", require("./routes/transactions"));
-  lotteryRouter.use("/items", require("./routes/items"));
-  lotteryRouter.use("/publicNotice", require("./routes/publicNotice"));
-  lotteryRouter.use("/quests", require("./routes/quests"));
+// [Router] APIs
+lotteryRouter.use("/globalState", require("./routes/globalState"));
+lotteryRouter.use("/transactions", require("./routes/transactions"));
+lotteryRouter.use("/items", require("./routes/items"));
+lotteryRouter.use("/publicNotice", require("./routes/publicNotice"));
+lotteryRouter.use("/quests", require("./routes/quests"));
 
-  // [AdminJS] AdminJS에 표시할 Resource 생성
-  const { buildResource } = require("../modules/adminResource");
-  const {
-    eventStatusModel,
-    questModel,
-    itemModel,
-    transactionModel,
-  } = require("./modules/stores/mongo");
-  const {
-    addOneItemStockAction,
-    addFiveItemStockAction,
-  } = require("./modules/items");
-
-  const resources = [
+// [AdminJS] AdminJS에 표시할 Resource 생성
+const resources =
+  (eventConfig && [
     buildResource()(eventStatusModel),
     buildResource()(questModel),
     buildResource([addOneItemStockAction, addFiveItemStockAction])(itemModel),
     buildResource()(transactionModel),
-  ];
+  ]) ||
+  [];
 
-  module.exports = {
-    lotteryRouter,
-    resources,
-    contracts: require("./modules/contracts"),
-  };
-} else {
-  module.exports = {
-    resources: [],
-  };
-}
+module.exports = {
+  lotteryRouter,
+  contracts,
+  resources,
+};
