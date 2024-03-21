4c884423ec4a2a3a95da41140fd833ae91a1b22a
ybmin <nimby@sparcs.org>
2024-03-21 17:18:16 +0900

    Docs: change to naver api optional

--- loadenv.js
+++ loadenv.js
@@ -55,7 +55,6 @@ module.exports = {
       endAt: "2024-03-19T00:00:00+09:00",
     },
   }, // optional
-  // Naver Cloud Platform Maps Directions 5 API Keys
-  naverCloudApiId: process.env.NAVER_MAP_API_ID, //required
-  naverCloudApiKey: process.env.NAVER_MAP_API_KEY, //required
+  naverCloudApiId: process.env.NAVER_MAP_API_ID, // optional
+  naverCloudApiKey: process.env.NAVER_MAP_API_KEY, //optional
 };
