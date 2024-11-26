4fea34dc71c2a2d70a2c64047739f77fc812e913
static <kmc7468@naver.com>
2024-11-26 21:59:38 +0900

    Refactor: apply new coding conventions on loadenv.ts

--- src/loadenv.ts
+++ src/loadenv.ts
@@ -1,12 +1,12 @@
 // 환경 변수에 따라 .env.production 또는 .env.development 파일을 읽어옵니다.
 import dotenv from "dotenv";
-import { type Algorithm } from "jsonwebtoken";
-import { type ServiceAccount } from "firebase-admin";
+import type { ServiceAccount } from "firebase-admin";
+import type { Algorithm } from "jsonwebtoken";
 
 if (process.env.NODE_ENV === undefined) {
   // logger.ts가 아직 초기화되지 않았으므로 console.error를 사용합니다.
   // eslint-disable-next-line no-console
-  console.error("NODE_ENV 환경변수가 설정되어 있지 않습니다.");
+  console.error("There is no NODE_ENV environment variable.");
   process.exit(1);
 }
 dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
@@ -14,7 +14,7 @@ dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 if (process.env.DB_PATH === undefined) {
   // logger.ts가 아직 초기화되지 않았으므로 console.error를 사용합니다.
   // eslint-disable-next-line no-console
-  console.error("DB_PATH 환경변수가 설정되어 있지 않습니다.");
+  console.error("There is no DB_PATH environment variable.");
   process.exit(1);
 }
 
@@ -29,7 +29,7 @@ export const sparcssso = {
   id: process.env.SPARCSSSO_CLIENT_ID || "", // optional
   key: process.env.SPARCSSSO_CLIENT_KEY || "", // optional
 };
-export const port = process.env.PORT ? parseInt(process.env.PORT) : 80; // optional (default = 80)
+export const port = parseInt(process.env.PORT || "80", 10); // optional (default = 80)
 export const corsWhiteList = (process.env.CORS_WHITELIST &&
   (JSON.parse(process.env.CORS_WHITELIST) as string[])) || [true]; // optional (default = [true])
 export const aws = {
@@ -41,7 +41,7 @@ export const aws = {
     `https://${process.env.AWS_S3_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com`, // optional
 };
 export const jwt = {
-  secretKey: process.env.JWT_SECRET_KEY || "TAXI_JWT_KEY",
+  secretKey: process.env.JWT_SECRET_KEY || "TAXI_JWT_KEY", // optional
   option: {
     algorithm: "HS256" as Algorithm,
     // FIXME: remove FRONT_URL from issuer. 단, issuer를 변경하면 이전에 발급했던 모든 JWT가 무효화됩니다.
