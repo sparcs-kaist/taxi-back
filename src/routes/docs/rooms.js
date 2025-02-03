f275b149b4a93894b2d9358f4cddaeb3a07b056a
TaehyeonPark <devtaehyeon@gmail.com>
2025-02-03 21:05:19 +0900

    Add: autoProcessingRooms

--- src/routes/docs/rooms.js
+++ src/routes/docs/rooms.js
@@ -645,7 +645,8 @@ roomsDocs[`${apiPrefix}/searchByUser`] = {
     summary: "사용자가 참여 중인 방 검색",
     description: `로그인 된 사용자가 참여 중인 방을 검색합니다.<br/>
     정산 완료 여부 기준으로 진행 중인 방과 완료된 방을 \`ongoing\`과 \`done\`으로 각각 분리하여 응답을 전송합니다.<br/>
-    (\`ongoing\`은 \`isOver\`이 flase인 방, \`done\`은 \`isOver\`이 true인 방을 의미합니다.)`,
+    (\`ongoing\`은 \`isOver\`이 flase인 방, \`done\`은 \`isOver\`이 true인 방을 의미합니다.)<br/>
+    혼자 참여 중인 방 중 이미 출발한 방이 있으면 정산 완료 처리한 후 과거 참여 방으로 옮긴다.`,
     parameters: {},
     responses: {
       200: {
