a83673bd7873e1f92b4465c83a6f8ae1ec0d1ab1
static <kmc7468@naver.com>
2026-02-27 03:53:03 +0900

    Fix: dictionary.txt unavailable in Docker

--- src/miniGame/modules/dictionary.ts
+++ src/miniGame/modules/dictionary.ts
@@ -6,7 +6,7 @@ const BATCH_SIZE = 1000;
 
 export const getDictionary = async () => {
   try {
-    const fileContent = await fs.readFile("../dictionary.txt", "utf-8");
+    const fileContent = await fs.readFile("data/dictionary.txt", "utf-8");
 
     const words = fileContent
       .split("\n")
